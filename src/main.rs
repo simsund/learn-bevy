988598697b7ee22dc9709714f3ddec293d6b9b20
simsund <127139770+simsund@users.noreply.github.com>
2024-03-30 18:15:43 +0100

    7
    Covered/Learned:
    
    First game setup
    Assets
    Bundles
    Resources
    2D Rendering
    More commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_player, spawn_camera))
+        .run()
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
