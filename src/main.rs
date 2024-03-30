b89206ba5cc8550fb1b8d92e10480823e7ce4942
simsund <127139770+simsund@users.noreply.github.com>
2024-03-30 23:01:14 +0100

    9
    Covered/Learned
    
    Input
    Player Movement
    More Systems
    More Queries
    Spawning Enemies

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+use rand::prelude::*;
+
+pub const PLAYER_SPEED: f32 = 500.0;
+pub const PLAYER_SIZE: f32 = 64.0;
+pub const NUMBER_OF_ENEMIES: usize = 20;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_player, spawn_camera))
-        .add_systems(Update, player_movement)
+        .add_systems(Startup, (spawn_player, spawn_camera, spawn_enemies))
+        .add_systems(Update, (player_movement, confine_player_movement))
         .run()
 }
 
 #[derive(Component)]
 pub struct Player {}
+#[derive(Component)]
+pub struct Enemy {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -38,7 +45,27 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     });
 }
 
-pub const PLAYER_SPEED: f32 = 500.0;
+pub fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {},
+        ));
+    }
+}
 
 pub fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
@@ -58,13 +85,42 @@ pub fn player_movement(
             direction += Vec3::new(0.0, -1.0, 0.0)
         }
         if keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight) {
-            direction += Vec3::new(0.0, 0.0, 1.0)
+            direction += Vec3::new(1.0, 0.0, 0.0)
         }
 
-        if direction.length() > 0.0 {
-            direction = direction.normalize_or_zero();
-        }
+        direction = direction.normalize_or_zero();
 
         transform.translation += direction * PLAYER_SPEED * time.delta_seconds()
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 2.0;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_transform.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_transform.translation = translation
     }
 }
