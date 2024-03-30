2028c4d466848c1d71c16ad7b921f4619e9fe258
simsund <127139770+simsund@users.noreply.github.com>
2024-03-30 03:00:37 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,25 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_systems(Startup, setup)
-        .add_systems(Update, print_names)
+        .add_systems(Update, people_with_jobs)
+        .add_systems(Update, people_without_job)
+        .add_systems(Update, people_ready_for_hire)
         .run()
 }
 
 pub fn setup(mut commands: Commands) {
-    commands.spawn(Person {
-        name: "Alex 1".to_string(),
-    });
-    commands.spawn(Person {
-        name: "Alex 2".to_string(),
-    });
+    commands.spawn((
+        Person {
+            name: "Alex 1".to_string(),
+        },
+        Employed { job: Job::Doctor },
+    ));
+    commands.spawn((
+        Person {
+            name: "Alex 2".to_string(),
+        },
+        Employed { job: Job::Lawyer },
+    ));
     commands.spawn(Person {
         name: "Alex 3".to_string(),
     });
@@ -28,6 +36,24 @@ pub fn setup(mut commands: Commands) {
 pub fn print_names(person_query: Query<&Person>) {
     for person in person_query.iter() {
         println!("Name: {}", person.name);
+    }
+}
+
+pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} has a job.", person.name)
+    }
+}
+
+pub fn people_without_job(person_query: Query<&Person, Without<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} does not have a job.", person.name)
+    }
+}
+
+pub fn people_ready_for_hire(person_query: Query<&Person, Without<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} are ready for hire.", person.name)
     }
 }
 
