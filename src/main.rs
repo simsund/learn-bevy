4532490e1c0f26c82c5c03fe8e8f6f04ab81f422
simsund <127139770+simsund@users.noreply.github.com>
2024-03-30 19:52:51 +0100

    8

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_player, spawn_camera))
+        .add_systems(Update, player_movement)
         .run()
 }
 
@@ -35,4 +36,35 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
+}
+
+pub const PLAYER_SPEED: f32 = 500.0;
+
+pub fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp) {
+            direction += Vec3::new(0.0, 1.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) || keyboard_input.pressed(KeyCode::ArrowLeft) {
+            direction += Vec3::new(-1.0, 0.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown) {
+            direction += Vec3::new(0.0, -1.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight) {
+            direction += Vec3::new(0.0, 0.0, 1.0)
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize_or_zero();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds()
+    }
 }
