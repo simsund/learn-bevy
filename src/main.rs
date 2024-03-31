504237afad34fab38960c4869400e10f744e015e
simsund <127139770+simsund@users.noreply.github.com>
2024-03-31 07:03:59 +0200

    11

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::audio::AudioBundle;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::prelude::*;
@@ -6,13 +7,23 @@ pub const PLAYER_SPEED: f32 = 500.0;
 pub const PLAYER_SIZE: f32 = 64.0;
 
 pub const NUMBER_OF_ENEMIES: usize = 20;
-pub const ENEMY_SPEED: f32 = 1.0;
+pub const ENEMY_SPEED: f32 = 100.0;
+pub const ENEMY_SIZE: f32 = 64.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_player, spawn_camera, spawn_enemies))
-        .add_systems(Update, (player_movement, confine_player_movement))
+        .add_systems(
+            Update,
+            (
+                player_movement,
+                confine_player_movement,
+                enemy_movement,
+                update_enemy_direction,
+                confine_enemy_movement,
+            ),
+        )
         .run()
 }
 
@@ -22,6 +33,9 @@ pub struct Player {}
 pub struct Enemy {
     pub direction: Vec2,
 }
+
+#[derive(Component)]
+pub struct EnemySoundQueue {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -108,26 +122,14 @@ pub fn confine_player_movement(
         let window = window_query.get_single().unwrap();
 
         let half_player_size = PLAYER_SIZE / 2.0;
-        let x_min = 0.0 + half_player_size;
-        let x_max = window.width() - half_player_size;
-        let y_min = 0.0 + half_player_size;
-        let y_max = window.height() - half_player_size;
+        let min = Vec2::splat(half_player_size).extend(f32::MIN);
+        let max = Vec3::new(
+            window.width() - half_player_size,
+            window.height() - half_player_size,
+            f32::MAX,
+        );
 
-        let mut translation = player_transform.translation;
-
-        if translation.x < x_min {
-            translation.x = x_min;
-        } else if translation.x > x_max {
-            translation.x = x_max;
-        }
-
-        if translation.y < y_min {
-            translation.y = y_min;
-        } else if translation.y > y_max {
-            translation.y = y_max;
-        }
-
-        player_transform.translation = translation
+        player_transform.translation = player_transform.translation.clamp(min, max);
     }
 }
 
@@ -136,4 +138,63 @@ pub fn enemy_movement(mut enemy_query: Query<(&mut Transform, &Enemy)>, time: Re
         let direction = Vec3::new(enemy.direction.x, enemy.direction.y, 0.0);
         transform.translation += direction * ENEMY_SPEED * time.delta_seconds();
     }
+}
+
+pub fn update_enemy_direction(
+    mut enemy_query: Query<(&Transform, &mut Enemy)>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    if let Ok(window) = window_query.get_single() {
+        let half_enemy_size = ENEMY_SIZE / 2.0;
+        let x_min = 0.0 + half_enemy_size;
+        let x_max = window.width() - half_enemy_size;
+        let y_min = 0.0 + half_enemy_size;
+        let y_max = window.height() - half_enemy_size;
+
+        for (transform, mut enemy) in enemy_query.iter_mut() {
+            let mut direction_changed = false;
+
+            let translation = transform.translation;
+            if translation.x < x_min || translation.x > x_max {
+                enemy.direction.x *= -1.0;
+                direction_changed = true;
+            }
+            if translation.y < y_min || translation.y > y_max {
+                enemy.direction.y *= -1.0;
+                direction_changed = true;
+            }
+
+            if direction_changed {}
+        }
+    }
+}
+
+pub fn confine_enemy_movement(
+    mut enemy_query: Query<&mut Transform, With<Enemy>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut enemy_transform) = enemy_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_enemy_size = ENEMY_SIZE / 2.0;
+        let min = Vec2::splat(half_enemy_size).extend(f32::MIN);
+        let max = Vec3::new(
+            window.width() - half_enemy_size,
+            window.height() - half_enemy_size,
+            f32::MAX,
+        );
+
+        enemy_transform.translation = enemy_transform.translation.clamp(min, max);
+    }
+}
+
+pub fn enemy_sound_queue(asset_server: Res<AssetServer>, mut commands: Commands) {
+    commands.spawn((
+        AudioBundle {
+            source: asset_server.load("audio/pluck_001.ogg"),
+            ..default()
+        },
+        EnemySoundQueue {},
+    ));
 }
